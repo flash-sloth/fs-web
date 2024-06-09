b990e5ffff03aadae279453e328007c3753f9b96
liyeo <423770406@qq.com>
2024-06-09 20:58:19 +0800

    build(components): 将vxe-table懒加载

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueDevtools from 'vite-plugin-vue-devtools';
 import progress from 'vite-plugin-progress';
+import { VxeResolver, lazyImport } from 'vite-plugin-lazy-import';
 import { setupElegantRouter } from './router';
 import { setupUnocss } from './unocss';
 import { setupUnplugin } from './unplugin';
@@ -13,6 +14,17 @@ export function setupVitePlugins(viteEnv: Env.ImportMeta) {
       script: {
         defineModel: true
       }
+    }),
+    // 配置vxe懒加载，减少首页加载时间
+    lazyImport({
+      resolvers: [
+        VxeResolver({
+          libraryName: 'vxe-table'
+        }),
+        VxeResolver({
+          libraryName: 'vxe-pc-ui'
+        })
+      ]
     }),
     vueJsx(),
     VueDevtools(),
