818a6581353319e58bbdd340c15123b9716f3d21
liyeo <423770406@qq.com>
2024-07-08 20:25:01 +0800

    refactor(api): 删除api目录下的问及那

--- src/api/main/generator/codeCreator.ts
+++
@@ -1,9 +0,0 @@
-import { RequestEnum } from '@vben/http';
-import { defHttp } from '@/service/http';
-
-export const preview = (ids: string[]) =>
-  defHttp.request<any[]>({
-    url: `/main/codeCreator/preview`,
-    method: RequestEnum.POST,
-    params: { ids, reload: true }
-  });
