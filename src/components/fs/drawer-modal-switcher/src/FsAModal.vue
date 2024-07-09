c659a8ca89455608f4740008a0cbb5afe3bedf82
liyeo <423770406@qq.com>
2024-07-10 00:07:20 +0800

    feat(fs-amodal): add fullscreen functionality and improve modal styling
    - Implement fullscreen toggle feature for FsAModal component.
    - Add modal header customization with title and close button.
    - Enhance modal styles for fullscreen mode and overall presentation.
    - Refactor code to use computed properties and improve component setup.

--- src/components/fs/drawer-modal-switcher/src/FsAModal.vue
+++ src/components/fs/drawer-modal-switcher/src/FsAModal.vue
@@ -1,12 +1,30 @@
 <script lang="ts" setup>
-import { getCurrentInstance, onMounted, ref } from 'vue';
+import { computed, getCurrentInstance, onMounted, ref } from 'vue';
 import { omit } from 'lodash';
 import { Modal } from 'ant-design-vue';
+import { CloseOutlined } from '@ant-design/icons-vue';
+import FullScreen from '@/components/common/full-screen.vue';
+import { $t } from '@/locales';
 import type { DmSwitcherInnerMethods } from './typing';
-
+const props = withDefaults(
+  defineProps<{
+    title?: string;
+    fullscreen?: boolean;
+    canFullscreen?: boolean;
+    warpClassName?: string;
+    closable?: boolean;
+  }>(),
+  {
+    title: '',
+    fullscreen: false,
+    canFullscreen: true,
+    warpClassName: '',
+    closable: true
+  }
+);
 const wapperRef = ref();
 const visible = ref(false);
-
+const isFullscreen = ref<boolean>(false);
 const emit = defineEmits<{
   register: [modalInstance: DmSwitcherInnerMethods, uuid: number];
 }>();
@@ -24,13 +42,55 @@ onMounted(() => {
     emit('register', modalMethods, instance.uid);
   }
 });
+function toggle() {
+  isFullscreen.value = !isFullscreen.value;
+}
+const warpClasses = computed(() => {
+  const classes = [];
+  if (props.fullscreen || isFullscreen.value) {
+    classes.push('fs-fullscreen-modal');
+  }
+  if (props.warpClassName) {
+    classes.push(props.warpClassName);
+  }
+  return classes.join(' ');
+});
 </script>
 
 <template>
-  <Modal ref="wapperRef" v-bind="$attrs" v-model:open="visible">
+  <Modal ref="wapperRef" v-bind="$attrs" v-model:open="visible" :closable="false" :wrap-class-name="warpClasses">
+    <template v-if="!$slots.title" #title>
+      <AFlex>
+        <div class="w-1 flex-1">{{ title }}</div>
+        <FullScreen v-if="canFullscreen" :full="isFullscreen" @click="toggle"></FullScreen>
+        <ButtonIcon v-if="closable" :tooltip-content="$t('common.close')" @click="visible = !visible">
+          <CloseOutlined />
+        </ButtonIcon>
+      </AFlex>
+    </template>
     <slot></slot>
     <template v-for="item in Object.keys(omit($slots, 'default'))" :key="item" #[item]="data">
       <slot :name="item" v-bind="data || {}"></slot>
     </template>
   </Modal>
 </template>
+
+<style lang="scss">
+.fs-fullscreen-modal {
+  overflow: hidden;
+  .ant-modal {
+    inset: 0 !important;
+    width: 100% !important;
+    max-width: 100%;
+    height: 100%;
+
+    &-content {
+      height: 100%;
+      overflow: hidden;
+    }
+  }
+  .ant-modal-footer {
+    margin-top: 0;
+  }
+}
+</style>
